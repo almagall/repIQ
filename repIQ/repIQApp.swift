57bbb64cd5ea8eb0f8e8c44f1e82b73c83e073cc
almagall <alfonso.m0010@gmail.com>
2026-03-02 23:58:08 -0800

    Initial Commit

---
+++ repIQ/repIQApp.swift
@@ -0,0 +1,17 @@
+//
+//  repIQApp.swift
+//  repIQ
+//
+//  Created by Alfonso Magallon on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct repIQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
